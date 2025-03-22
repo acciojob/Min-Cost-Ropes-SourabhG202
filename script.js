3ed870f4fe635951ac41eb142ad12d65fae37080
Sourabh G <154436409+SourabhG202@users.noreply.github.com>
2025-03-22 14:12:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+arr.sort((a,b)=>a-b);
+	let totalcost = 0;
+	while(arr.length>1){
+		let first = arr.shift();
+		let second = arr.shift();
+		let cost = first+second;
+		totalcost += cost;
+		arr.push(cost);
+		arr.sort((a,b)=>a-b);
+	}
+	return totalcost;
+	
+	
   
 }
 
